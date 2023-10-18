80a754d898d70ea525a1aa26e7ced3f1e1b5ba51
guifabre <guijoseph.fabre@gmail.com>
2023-10-18 13:19:04 -0400

    import doc to reexport

--- R/00-import-from-madshapR.R
+++ R/00-import-from-madshapR.R
@@ -1,91 +1,167 @@
 #' @importFrom madshapR as_data_dict
+#' @name as_data_dict
+#' @rdname as_data_dict
+#' @keywords imported
 #' @export
 madshapR::as_data_dict
 
 #' @importFrom madshapR is_data_dict_mlstr
+#' @name is_data_dict
+#' @rdname is_data_dict
+#' @keywords imported
 #' @export
 madshapR::is_data_dict
 
 #' @importFrom madshapR as_data_dict_mlstr
+#' @name as_data_dict_mlstr
+#' @rdname as_data_dict_mlstr
+#' @keywords imported
 #' @export
 madshapR::as_data_dict_mlstr
 
 #' @importFrom madshapR is_data_dict
+#' @name is_data_dict_mlstr
+#' @rdname is_data_dict_mlstr
+#' @keywords imported
 #' @export
 madshapR::is_data_dict_mlstr
 
 #' @importFrom madshapR as_dataset
+#' @name as_dataset
+#' @rdname as_dataset
+#' @keywords imported
 #' @export
 madshapR::as_dataset
 
 #' @importFrom madshapR is_dataset
+#' @name is_dataset
+#' @rdname is_dataset
+#' @keywords imported
 #' @export
 madshapR::is_dataset
 
 #' @importFrom madshapR as_dossier
+#' @name as_dossier
+#' @rdname as_dossier
+#' @keywords imported
 #' @export
 madshapR::as_dossier
 
 #' @importFrom madshapR is_dossier
+#' @name is_dossier
+#' @rdname is_dossier
+#' @keywords imported
 #' @export
 madshapR::is_dossier
 
 #' @importFrom madshapR data_extract
+#' @name data_extract
+#' @rdname data_extract
+#' @keywords imported
 #' @export
 madshapR::data_extract
 
 #' @importFrom madshapR data_dict_extract
+#' @name data_dict_extract
+#' @rdname data_dict_extract
+#' @keywords imported
 #' @export
 madshapR::data_dict_extract
 
 #' @importFrom madshapR data_dict_apply
+#' @name data_dict_apply
+#' @rdname data_dict_apply
+#' @keywords imported
 #' @export
 madshapR::data_dict_apply
 
 #' @importFrom madshapR dataset_zap_data_dict
+#' @name dataset_zap_data_dict
+#' @rdname dataset_zap_data_dict
+#' @keywords imported
 #' @export
 madshapR::dataset_zap_data_dict
 
 #' @importFrom madshapR dossier_create
+#' @name dossier_create
+#' @rdname dossier_create
+#' @keywords imported
 #' @export
 madshapR::dossier_create
 
 #' @importFrom madshapR dataset_evaluate
+#' @name dataset_evaluate
+#' @rdname dataset_evaluate
+#' @keywords imported
 #' @export
 madshapR::dataset_evaluate
 
 #' @importFrom madshapR data_dict_evaluate
+#' @name data_dict_evaluate
+#' @rdname data_dict_evaluate
+#' @keywords imported
 #' @export
 madshapR::data_dict_evaluate
 
 #' @importFrom madshapR dossier_evaluate
+#' @name dossier_evaluate
+#' @rdname dossier_evaluate
+#' @keywords imported
 #' @export
 madshapR::dossier_evaluate
 
 #' @importFrom madshapR dataset_summarize
+#' @name dataset_summarize
+#' @rdname dataset_summarize
+#' @keywords imported
 #' @export
 madshapR::dataset_summarize
 
 #' @importFrom madshapR dossier_summarize
+#' @name dossier_summarize
+#' @rdname dossier_summarize
+#' @keywords imported
 #' @export
 madshapR::dossier_summarize
 
 #' @importFrom madshapR dataset_visualize
+#' @name dataset_visualize
+#' @rdname dataset_visualize
+#' @keywords imported
 #' @export
 madshapR::dataset_visualize
 
 #' @importFrom madshapR valueType_adjust
+#' @name valueType_adjust
+#' @rdname valueType_adjust
+#' @keywords imported
 #' @export
 madshapR::valueType_adjust
 
 #' @importFrom madshapR as_taxonomy
+#' @name as_taxonomy
+#' @rdname as_taxonomy
+#' @keywords imported
 #' @export
 madshapR::as_taxonomy
 
 #' @importFrom madshapR is_taxonomy
+#' @name is_taxonomy
+#' @rdname is_taxonomy
+#' @keywords imported
 #' @export
 madshapR::is_taxonomy
 
 #' @importFrom madshapR bookdown_open
+#' @name bookdown_open
+#' @rdname bookdown_open
+#' @keywords imported
 #' @export
 madshapR::bookdown_open
+
+#' @importFrom madshapR bookdown_render
+#' @name bookdown_render
+#' @rdname bookdown_render
+#' @keywords imported
+#' @export
+madshapR::bookdown_render
